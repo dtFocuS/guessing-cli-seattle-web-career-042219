64593af85b21ebe2757f17e9311a55d6ad22d9a4
Danny Tseng <dtfocus1025@gmail.com>
2019-04-12 05:11:23 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     if user_input == computer_number
       puts "You guessed the correct number!"
     elsif user_input != computer_number
-      puts 
+      puts "The computer guessed #{computer_number}"
     end
       
   end
