0d859ad304730fba19aa98dab47c700bfd8a602e
Danny Tseng <dtfocus1025@gmail.com>
2019-04-12 05:14:11 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     computer_number = rand(1..6)
     user_input = gets.chomp
     if user_input == "exit"
-      puts "goodbye"
+      puts "Goodbye!"
     elsif user_input == computer_number
       puts "You guessed the correct number!"
     elsif user_input != computer_number
